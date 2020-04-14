de376f865a6262885dbd78f2c43d21aadf130be1
KseniaChezz <kseniachezz@yandex.ru>
2020-04-14 21:47:03 +0300

    добавление картинок, цветов, текстовых констант

---
+++ src/constants/colors.ts
@@ -0,0 +1,18 @@
+export const COLORS = {
+    GreyBlue: 'rgb(205, 210, 220)',
+    LightBlue: 'rgb(172, 187, 210)',
+    Blue: 'rgb(77, 146, 223)',
+    BrightBlue: 'rgb(131, 175, 240)',
+    BlueTransparent:'rgba(219, 233, 248, 0.5)',
+    WhiteGrey: 'rgb(247, 247, 247)',
+    White: 'rgb(255, 255, 255)',
+    DarkGrey: 'rgb(66, 66, 66)',
+    LightGrey: 'rgb(189,189,189)',
+    Border: 'rgb(238, 238, 238)',
+    Yellow: 'rgb(255, 225, 120)',
+    Red: 'rgb(207, 94, 94)',
+    TagLightBlue: 'rgb(147, 184, 239)',
+    TagBlue: 'rgb(95, 144, 218)',
+    TagBlueViolet: 'rgb(87, 105, 195)',
+    TadDarkBlue: 'rgb(44, 65, 135)',
+};
